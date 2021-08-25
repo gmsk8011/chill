5d726bf7156fae65f18d1d204288306faf1093bb
gmsk8011 <79661563+gmsk8011@users.noreply.github.com>
2021-08-26 01:09:56 +0530

    done

---
+++ site/js/index.js
@@ -0,0 +1,69 @@
+a = new Date();
+function hr() {
+  setInterval(() => {
+    a = new Date();
+    if (a.getSeconds() < 10) {
+      sec = a.getSeconds() + 10;
+    }
+    else
+     sec = a.getSeconds();
+    if (a.getMinutes() < 10) {
+      min = a.getMinutes() + 10;
+    }
+    else
+     min = a.getMinutes();
+    
+    let time = a.getHours() + ':' + min + ':' + sec + ' AM';
+    let tim = 'Time ' + a.getHours() + ':' + min + ' AM';
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('tab').innerHTML = tim;
+  }, 1000);   
+}
+function hrr() {
+  setInterval(() => {
+    a = new Date();
+    if (a.getSeconds() < 10) {
+      sec = '0' + a.getSeconds();
+    }
+    else
+     sec = a.getSeconds();
+    if (a.getMinutes() < 10) {
+      min = '0' + a.getMinutes();
+    }
+    else
+     min = a.getMinutes();
+    
+    let time = a.getHours() - 12 + ':' + min + ':' + sec + ' PM';
+    let tim = 'Time ' + a.getHours() - 12 + ':' + min;
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('tab').innerHTML = tim;
+  }, 1000);
+}
+function hrrr() {
+  setInterval(() => {
+    a = new Date();
+    if (a.getSeconds() < 10) {
+      sec = '0' + a.getSeconds();
+    }
+    else
+     sec = a.getSeconds();
+    if (a.getMinutes() < 10) {
+      min = '0' + a.getMinutes();
+    }
+    else
+     min = a.getMinutes();
+    
+    let time = a.getHours() + 12 + ':' + min + ':' + sec + ' AM';
+    let tim = a.getHours() + 12 + ':' + min;      document.getElementById('time').innerHTML = time;
+    document.getElementById('tab').innerHTML = tim;
+  }, 1000);
+}
+if (a.getHours() <= 12) {
+  hr();
+}
+else if (a.getHours() > 12) {
+  hrr();
+}
+else if (a.getHours() == 0) {
+  hrrr();
+}
